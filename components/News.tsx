38a04bea67a1831ad37d598158fa0c2ff45fa963
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-27 00:31:03 -0400

    fix: url

--- components/News.tsx
+++ components/News.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement } from "react";
 
 const articles: ArticleProps[] = [
-    { src: '/pj.png', title: 'Projen: The Next CDK Surprise!', blurb: 'State of the Union Why projen That\'s a really good question, isn\'t it? After all, there is a new templator or scaffolder practically daily.', categories: [ 1, 5, 34 ], url: '#' },
-    { src: '/pj.png', title: 'Est elit et irure ullamco aliqua anim anim commodo irure id sint fugiat quis ea.', blurb: 'Exercitation cupidatat dolore et occaecat proident.', categories: [ 19 ], url: '#' },
-     {src: '/pj.png', title: 'Id laborum ex qui dolor voluptate sint in pariatur ea.', blurb: 'Quis ullamco pariatur laborum non occaecat.', categories: [ 80, 81, 82 ], url: '#' },
-    { src: '/pj.png', title: 'Id laborum ex qui dolor voluptate sint in pariatur ea.', blurb: 'Quis ullamco pariatur laborum non occaecat.', categories: [ 80, 81, 82 ], url: '#' },
+    { url: 'https://dev.to/aws-builders/projen-the-next-cdk-suprise-13b2', src: '/pj.png', title: 'Projen: The Next CDK Surprise!', blurb: 'State of the Union Why projen That\'s a really good question, isn\'t it? After all, there is a new templator or scaffolder practically daily.', categories: [ 1, 5, 34 ] },
+    { src: '/pj.png', title: 'Est elit et irure ullamco aliqua anim anim commodo irure id sint fugiat quis ea.', blurb: 'Exercitation cupidatat dolore et occaecat proident.', categories: [ 19 ] },
+    { src: '/pj.png', title: 'Id laborum ex qui dolor voluptate sint in pariatur ea.', blurb: 'Quis ullamco pariatur laborum non occaecat.', categories: [ 80, 81, 82 ] },
+    { src: '/pj.png', title: 'Id laborum ex qui dolor voluptate sint in pariatur ea.', blurb: 'Quis ullamco pariatur laborum non occaecat.', categories: [ 80, 81, 82 ] },
 ];
 
 export interface ArticleProps {
@@ -20,7 +20,9 @@ const Article = (article: ArticleProps): ReactElement => {
   return (
     <div className="flex flex-col w-full overflow-hidden rounded-lg shadow-lg">
       <div className="flex-shrink-0 h-48 ml-20">
-        <a href={article.url}><img className="h-48" src={article.src} /></a>
+        <a href={article.url}>
+          <img className="h-48" src={article.src} />
+        </a>
       </div>
       <div className="flex flex-col justify-between flex-1 p-6 bg-white">
         <div className="flex-1">
@@ -53,7 +55,7 @@ const News = (): ReactElement => (
   <div className="grid max-w-lg gap-5 mx-auto mt-0 lg:grid-cols-4 lg:max-w-none">
     {
       articles.map((article: ArticleProps) => (
-        <Article key={article.title} src={article.src} title={article.title} blurb={article.blurb} categories={article.categories} />
+        <Article key={article.title} src={article.src} title={article.title} blurb={article.blurb} url={article.url} categories={article.categories} />
       ))
       }
   </div>
