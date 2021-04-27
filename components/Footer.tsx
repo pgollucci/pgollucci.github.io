9f8674b1fa98315c4c956334e38fea2080557681
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-26 21:05:23 -0400

    chore: sort the footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from "react";
 
 const social = [
+  { name: 'Dev.to', href: 'https://dev.to/pgollucci', icon: () => (<p>Dev.to</p>), },
   { name: 'GitHub', href: 'https://github.com/pgollucci', icon: () => (<p>Github</p>), },
-  { name: 'Twitter', href: 'https://github.com/p6m7g8', icon: () => (<p>Twitter</p>), },
-  { name: 'Youtube', href: 'https://www.youtube.com/channel/UC6mOzHytcWLRJInP5B_Qg6g', icon: () => (<p>Youtube</p>), },
   { name: 'LinkedIn', href: 'https://www.linkedin.com/in/pgollucci/', icon: () => (<p>LinkedIn</p>), },
   { name: 'Medium', href: 'https://medium.com/@pgollucci', icon: () => (<p>Medium</p>), },
-  { name: 'Dev.to', href: 'https://dev.to/pgollucci', icon: () => (<p>Dev.to</p>), },
+  { name: 'Twitter', href: 'https://github.com/p6m7g8', icon: () => (<p>Twitter</p>), },
+  { name: 'Youtube', href: 'https://www.youtube.com/channel/UC6mOzHytcWLRJInP5B_Qg6g', icon: () => (<p>Youtube</p>), },
 ];
 
 const Footer = (): ReactElement => (
