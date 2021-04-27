28c62257df2b4b8aa0c78668c65a2a14f7565296
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-26 23:49:06 -0400

    chore: continue stubbing

--- components/About.tsx
+++ components/About.tsx
@@ -2,35 +2,68 @@ import React, { ReactElement } from "react"
 
 const About = (): ReactElement =>(
   <>
-    <div className="grid grid-flow-row gap-4 mt-8 ml-8 mr-8 lg:grid-rows-2 lg:grid-cols-2">
-      <div className="flex flex-col">
-        <div className="flex flex-row gap-8 mx-auto">
-          <div className=""><img className="h-52 w-52 rounded-2xl" src="/animal.jpg" /></div>
-          <div className="my-auto"><img className="h-20" src="/ldar.png" /></div>
+    <div className="grid grid-flow-row gap-8 mt-8 ml-8 mr-8 lg:grid-cols-3 xl:grid-cols-4">
+      <div>
+        <div className="flex flex-col h-64">
+          <div className="mx-auto"><img className="w-64 h-48 rounded-2xl" src="/animal.jpg" /></div>
+          <div className="mx-auto"><img className="w-64 h-12 rounded-2xl" src="/ldar.png" /></div>
         </div>
-        <div>
-          <div className="mx-auto mt-8 prose-sm prose">
-            <h4>Part of a mixed paid/volunteer team responsible for saving cats and dogs all over the world. Over 20k dogs and cats have been saved from over 10 states and 5 countries since 2009.</h4>
-            <ul>
-              <li>Fostered over 200 dogs.</li>
-              <li>Represented Lucky Dog Animal Rescue at AWS Worldwide Public Sector Summit in 2019.</li>
-              <li>1st place national fund-raising team for Best Friends Society.</li>
-              <li>National crisis response to all US hurricanes in 2018.</li>
-              <li>Puerto Rico earthquake response in 2019: 14,000 pounds of food & water, and rescue of 78 dogs.</li>
-              <li>Puerto Rico COVID-19 2020 foster & recuse of 62 dogs.</li>
-              <li>Traveled to Puerto Rico, Thailand, and Hawaii (for Kauai Humane Society) rescuing animals from high kill shelters.</li>
-            </ul>
-          </div>
+        <div className="mt-2 prose-sm prose">
+          <h4>Part of a mixed paid/volunteer team responsible for saving cats and dogs all over the world. Over 20k dogs and cats have been saved from over 10 states and 5 countries since 2009.</h4>
+          <ul>
+            <li>Fostered over 200 dogs.</li>
+            <li>Represented Lucky Dog Animal Rescue at AWS Worldwide Public Sector Summit in 2019.</li>
+            <li>1st place national fund-raising team for Best Friends Society.</li>
+            <li>National crisis response to all US hurricanes in 2018.</li>
+            <li>Puerto Rico earthquake response in 2019: 14,000 pounds of food & water, and rescue of 78 dogs.</li>
+            <li>Puerto Rico COVID-19 2020 foster & recuse of 62 dogs.</li>
+            <li>Traveled to Puerto Rico, Thailand, and Hawaii (for Kauai Humane Society) rescuing animals from high kill shelters.</li>
+          </ul>
         </div>
       </div>
-      <div className="h-96">
-        2
+      <div>
+        <div className="h-64">
+          <img className="h-48 mx-auto w-96" src="/awscb.png" />
+        </div>
+        <div className="mt-2 prose-sm prose">
+          <h4 className="text-center">
+            One of only ~800 World Wide in this VIP AWS Program.<br />
+            DevTools (63)<br />
+            Management, Migration & Governance (71)</h4>
+          <ul>
+            <li>Lead winning (1st place) World Wide Public Sector Team, May 2020</li>
+            <li>Placed in the top 0.001% of AWS Challenges at Worldwide Public Sector AWS Summit June 30, 2020</li>
+            <li>AWS SysOps Associate Exam Contributor, November 2019</li>
+            <li>AWS Security Specialty Exam Question Author,, May 2021</li>
+            <li>CDK.dev member, AWS CDK, CDK8s, CDKtf, and projen contributor</li>
+            <li>Member, AWS Worldwide Public Sector Partner Advisory Council</li>
+            <li>Member, AWS IQ Experts (verified)</li>
+          </ul>
+        </div>
       </div>
-      <div className="h-96">
-        3
+      <div>
+        <div className="h-64">
+          <img className="mx-auto h-38" src="/fbsd.jpg" />
+        </div>
+        <div className="mt-2 prose-sm prose">
+          <h4>18th most prolific FreeBSD ports committer World Wide.</h4>
+          <ul>
+            <li>Instrumental in Apache Software Foundation, ruby, and perl ports</li>
+          </ul>
+        </div>
       </div>
-      <div className="h-96">
-        4
+      <div>
+        <div className="h-64">
+          <img className="mx-auto" src="/asf.png" />
+        </div>
+        <div className="mt-2 prose-sm prose">
+          <h4>Vice President of Apache Software Foundation Infrastructure (World Wide)</h4>
+          <ul>
+            <li>Held root@ for 4yrs</li>
+            <li>Managed Global Infrastructure Budget, Data Centers, and Staff</li>
+            <li>Project Management Committee: httpd, apr, apreq, mod_perl</li>
+          </ul>
+        </div>
       </div>
     </div>
   </>
--- components/Pitch.tsx
+++ components/Pitch.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react"
 
 const Pitch = (): ReactElement =>(
   <>
-    <div className="my-auto ml-4 mr-4 text-2xl prose">
+    <div className="my-auto ml-4 mr-4 text-2xl prose shadow-xl">
       20+ years of expertise building teams that focus on results. Cross functional leader with a focus on the customer. Acumen for simplifying processes with technology to deliver on innovation and increase return on investment (ROI). A trusted advisor, fostering relationships across functions and lines of business to facilitate organizational needs.
     </div>
   </>
