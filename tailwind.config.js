d3b06d67f2dba5db5200586a512a947fb9c19454
Laravel Shift <shift@laravelshift.com>
2021-05-04 22:02:19 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,8 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  theme: {
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            'max-width': 'full',
-          }
-        }
-      })
-    }
-  },
-  purge: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-  ],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require(`@tailwindcss/ui`),
-  ],
-  colors: {
-    primary: '#51c4d3',
-    secondary: '#004e89',
-  },
+  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  darkMode: 'media',
+  plugins: [require('@tailwindcss/forms'), require(`@tailwindcss/ui`)],
+  colors: { primary: '#51c4d3', secondary: '#004e89' },
 }
