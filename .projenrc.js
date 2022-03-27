cd3a78d786cf46c9ffb45702f94257c28b30de91
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-27 18:34:28 -0400

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,12 @@
+const { web } = require("projen");
+const project = new web.NextJsTypeScriptProject({
+  defaultReleaseBranch: "main",
+  name: "pgollucci.github.io",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // tailwind: true,          /* Setup Tailwind CSS as a PostCSS plugin. */
+});
+project.synth();
